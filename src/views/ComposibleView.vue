60425d0a8823b256437c82d71ddab554beb89215
Alexey <anelyubov93@gmail.com>
2024-10-20 22:59:45 +0300

    init logic prototype

--- src/views/ComposibleView.vue
+++ src/views/ComposibleView.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const counter = ref<number>(0)
+</script>
+
 <template>
   <div class="main-information">composible</div>
+  <div>
+    <span>Counter</span>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+    <span>{{ counter }}</span>
+  </div>
 </template>
